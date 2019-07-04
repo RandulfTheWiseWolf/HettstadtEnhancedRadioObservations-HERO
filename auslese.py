2ea93f1890c9524013fc3f5c5b96d01eb750f60b
RomanIotov <52529111+RomanIotov@users.noreply.github.com>
2019-07-04 12:49:28 +0200

    Add files via upload

--- auslese.py
+++ auslese.py
@@ -1,18 +1,5 @@
 #TODO: Remove for loop and onily operate on single lines.
-
-#filename = "azimut"
-#filename = "elevation"
-#filename = "Elevationh"
-filename = "Azimuth"
-
-i = 0
-
-
-
-f = open(filename, "r")
-
-sigAmps = []
-
+import pandas as pd
 
 def getTimes(lst):
     relatives = []
@@ -56,70 +43,92 @@ def cleanseAmplitude(line):
         
         return cleanAmps
     
+#filename = "azimut"
+#filename = "elevation"
+#filename = "Elevationh"
+filenames = ["azimut", "elevation", "Azimuth", "Elevationh"]
+savenames = ["Azimut", "Elevation", "Azimut-UFO", "Elevation-UFO"]
+
+
+filecounter=0
+
+for filename in filenames:
+    i = 0
+    f = open(filename, "r")
+
+    sigAmps = []
+    
+
+
+    
+
+    for line in f:
+        if i == 6:
+            cleanFreq = cleanseFrequency(line)
+        
+        if i >= 9:
+            cleanAmps = cleanseAmplitude(line)
+        
+            sigAmps.append(cleanAmps)
+        
+        i += 1 
+
+
+
+    for i in range(0, len(cleanFreq)):
+        cleanFreq[i] = float(cleanFreq[i])	
+        
+        
+        
+    #lastAmp = cleanAmps.pop()
+    #cleanLastAmp = lastAmp.strip("\r\n")
+    #cleanAmps.append(cleanLastAmp)
+    
+    #Delete Time
+    #cleanAmps = cleanAmps[4::]
     
     
-
-for line in f:
-    if i == 6:
-        cleanFreq = cleanseFrequency(line)
+    
+    #print(cleanFreq)
+    #print(sigAmps)
+    
+    times = getTimes(sigAmps)
+    
+    a=0
+    for schmist in sigAmps:
+        sigAmps[a]=schmist[4:]
+        a+=1
+            
+    #print(times)
+            
+    #print(len(sigAmps[1]), len(cleanFreq),len(sigAmps))
+            
+    f.close()
+            
+    cleanFreqs=cleanFreq*len(times)
+    flatAmps=[]
+    for i in range(0,len(sigAmps)):
+        flatAmps=flatAmps+sigAmps[i]
         
-    if i >= 9:
-        cleanAmps = cleanseAmplitude(line)
-        
-        sigAmps.append(cleanAmps)
-        
-    i += 1 
-
-
-
-for i in range(0, len(cleanFreq)):
-    cleanFreq[i] = float(cleanFreq[i])	
-
-
-
-#lastAmp = cleanAmps.pop()
-#cleanLastAmp = lastAmp.strip("\r\n")
-#cleanAmps.append(cleanLastAmp)
-
-#Delete Time
-#cleanAmps = cleanAmps[4::]
-
-
-
-#print(cleanFreq)
-#print(sigAmps)
-
-times = getTimes(sigAmps)
-
-a=0
-for schmist in sigAmps:
-    sigAmps[a]=schmist[4:]
-    a+=1
-
-print(times)
-
-print(len(sigAmps[1]), len(cleanFreq),len(sigAmps))
-
-f.close()
-
-cleanFreqs=cleanFreq*len(times)
-flatAmps=[]
-for i in range(0,len(sigAmps)):
-    flatAmps=flatAmps+sigAmps[i]
-        
-ttimes=[]
-for time in times:
-    for i in range(0,len(sigAmps[0])):
-        ttimes.append(time)
-        
-#print(len(ttimes), len(flatAmps),len(cleanFreqs))   
-
-import pandas as pd
-
-Daten = {'Frequenz':cleanFreqs,'Amplitude':flatAmps, 'Zeit(s)':ttimes,}
-df = pd.DataFrame(Daten, columns=['Frequenz','Amplitude','Zeit(s)'])
-#df.to_csv('Elevation-UFO-Werte')
-df.to_csv('Azimut-UFO-Werte')
-#df.to_csv('Azimut-Werte')
-#df.to_csv('Elevation-Werte')
-print(df)
+    ttimes=[]
+    for time in times:
+        for i in range(0,len(sigAmps[0])):
+            ttimes.append(time)
+                        
+    #print(len(ttimes), len(flatAmps),len(cleanFreqs))   
+                        
+                        
+    Daten = {'Frequenz':cleanFreqs,'Amplitude':flatAmps, 'Zeit(s)':ttimes,}
+    df = pd.DataFrame(Daten, columns=['Frequenz','Amplitude','Zeit(s)'])
+    
+    finalsavename=savenames[filecounter]+"-Werte"
+    print(type(finalsavename))
+    df.to_csv(finalsavename)
+    
+    filecounter+=1
+    #df.to_csv('Elevation-UFO-Werte')
+    #df.to_csv('Azimut-UFO-Werte')
+    #df.to_csv('Azimut-Werte')
+    #df.to_csv('Elevation-Werte')
+    #print(df)
+                            
